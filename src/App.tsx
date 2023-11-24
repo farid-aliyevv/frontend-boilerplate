654aeea414ed8081161041d88ec02f75fa2647ae
Farid R. ALIYEV <farid.r.aliyev@socardownstream.az>
2023-11-24 11:28:10 +0400

    build: configured environments
    setup development and production environments, changed scripts and configured environment variables

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import './App.css';
 import { Counter } from 'context/counter/Counter';
 
 function App() {
+	console.log(import.meta.env.VITE_BACKEND_URL);
+
 	return (
 		<div className="App">
 			<header className="App-header">
